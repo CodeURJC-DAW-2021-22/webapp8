15f39f14f9167c9fa4733a74530a1efd5041b2c8
Wilhelm-mar <guillermo.mg.trabajo@gmail.com>
2022-02-22 14:03:55 +0100

    Basic entity

--- backend/webandtech/src/main/java/webapp8/webandtech/model/Rating.java
+++ backend/webandtech/src/main/java/webapp8/webandtech/model/Rating.java
@@ -1,5 +1,11 @@
 package webapp8.webandtech.model;
 
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "ratings")
 public class Rating {
 
 }
--- backend/webandtech/src/main/java/webapp8/webandtech/model/User.java
+++ backend/webandtech/src/main/java/webapp8/webandtech/model/User.java
@@ -1,5 +1,37 @@
 package webapp8.webandtech.model;
 
+import java.sql.Blob;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+@Table(name = "users")
 public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int idusers;
+	@Column
+	private String email;
+	@Column
+	private String username;
+	@Column
+	private String pass;
+	@Column
+	private String name;
+	@Lob
+	@JsonIgnore
+	private Blob userimg;
+	@Column
+	private boolean userprofile;
+	@Column
+	private String address;
 
 }

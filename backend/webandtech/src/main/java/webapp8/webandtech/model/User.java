801109ce9a2844ada2033ce54b32d49aa0646eac
Wilhelm-mar <guillermo.mg.trabajo@gmail.com>
2022-02-23 10:14:11 +0100

    Update user model

--- backend/webandtech/src/main/java/webapp8/webandtech/model/User.java
+++ backend/webandtech/src/main/java/webapp8/webandtech/model/User.java
@@ -11,13 +11,19 @@ import javax.persistence.Lob;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonView;
 
 @Entity
 @Table(name = "users")
 public class User {
+	
+	public interface Basic{}
+	public interface Detailed extends User.Basic{}
+	
+	@JsonView(Basic.class)
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private int idusers;
+	private int iduser;
 	@Column
 	private String email;
 	@Column
@@ -33,5 +39,67 @@ public class User {
 	private boolean userprofile;
 	@Column
 	private String address;
-
+	
+	public User() {}
+	public User(int iduser, String email, String username, String pass, String name, Blob userimg, boolean userprofile,
+			String address) {
+		super();
+		this.iduser = iduser;
+		this.email = email;
+		this.username = username;
+		this.pass = pass;
+		this.name = name;
+		this.userimg = userimg;
+		this.userprofile = userprofile;
+		this.address = address;
+	}
+	
+	public int getIduser() {
+		return iduser;
+	}
+	public void setIduser(int iduser) {
+		this.iduser = iduser;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPass() {
+		return pass;
+	}
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Blob getUserimg() {
+		return userimg;
+	}
+	public void setUserimg(Blob userimg) {
+		this.userimg = userimg;
+	}
+	public boolean isUserprofile() {
+		return userprofile;
+	}
+	public void setUserprofile(boolean userprofile) {
+		this.userprofile = userprofile;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
 }

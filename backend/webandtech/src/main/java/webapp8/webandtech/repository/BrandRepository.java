e0b15cf32d9cd92dff9d96fa8ea7b839e93444ca
Romofe01 <romfe01@gmail.com>
2022-03-01 10:35:59 +0100

    Add brand repository

---
+++ backend/webandtech/src/main/java/webapp8/webandtech/repository/BrandRepository.java
@@ -0,0 +1,19 @@
+package webapp8.webandtech.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import webapp8.webandtech.model.Brand;
+
+
+
+public interface BrandRepository extends JpaRepository<Brand, Integer>{
+	
+	public Optional<Brand> findBybrandname(String brandname);
+	public Optional<Brand> findByidbrand(String brandname);
+	public List<Brand> findAll();
+	boolean existsIdbrandsByBrandname(String brandname);
+
+}

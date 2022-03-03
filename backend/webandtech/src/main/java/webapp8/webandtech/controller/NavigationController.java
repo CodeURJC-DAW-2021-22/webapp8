61aac31e18327e4d74164ba6c72cfae404cdcc83
Guillermo <guillermo.mg.trabajo@gmail.com>
2022-03-03 12:00:36 +0100

    Update controller and service

--- backend/webandtech/src/main/java/webapp8/webandtech/controller/NavigationController.java
+++ backend/webandtech/src/main/java/webapp8/webandtech/controller/NavigationController.java
@@ -1,5 +1,41 @@
 package webapp8.webandtech.controller;
 
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import webapp8.webandtech.service.AdminService;
+import webapp8.webandtech.service.UserService;
+
+@Controller
+@CrossOrigin
 public class NavigationController {
+
+    @Autowired
+    private UserService userService;
+    private AdminService adminService;
+
+    @GetMapping("/")
+	private void getInitialPage(HttpServletResponse response) throws IOException {
+		response.sendRedirect("/index");		
+	}
+    @GetMapping("/login")
+	private String getSignIn(Model model,HttpServletRequest request) throws IOException {
+		//userService.loadDataBase();
+		return "login";
+	}
+    @GetMapping("/index")
+	private String getIndex(Model model,HttpServletRequest request) throws IOException {
+		//userService.loadDataBase();
+		return "index";
+	}
+
 
 }
---
+++ backend/webandtech/src/main/java/webapp8/webandtech/service/AdminService.java
@@ -0,0 +1,8 @@
+package webapp8.webandtech.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AdminService {
+    
+}

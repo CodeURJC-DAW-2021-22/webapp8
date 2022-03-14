f6aa77939bd8be4632f2db8a82a4c5f8729b7602
Guillermo <guillermo.mg.trabajo@gmail.com>
2022-03-14 03:30:54 +0100

    Update Rating

--- backend/webandtech/src/main/java/webapp8/webandtech/repository/RatingRepository.java
+++ backend/webandtech/src/main/java/webapp8/webandtech/repository/RatingRepository.java
@@ -2,6 +2,8 @@ package webapp8.webandtech.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import webapp8.webandtech.model.Product;
@@ -10,6 +12,7 @@ import webapp8.webandtech.model.User;
 
 public interface RatingRepository extends JpaRepository<Rating, Integer>{
     public List<Rating> findByidproduct(Product idproduct);
+    public Page<Rating> findByidproduct(Product idproduct, Pageable page);
 	public List<Rating> findByiduser(User iduser);
 	public Rating findByIdproductAndIduser(Product idproduct, User iduser);
 	public Long deleteByIduser(User iduser);

df54016451cc49b9c775cfa20aa13e7a8496f792
sergio-octavio <sergiooctaviomancebo@gmail.com>
2022-03-31 19:27:56 +0200

    Add UserRestControler

---
+++ backend/webandtech/src/main/java/webapp8/webandtech/controller/api/users/UserRestControler.java
@@ -0,0 +1,17 @@
+package webapp8.webandtech.controller.api.users;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import webapp8.webandtech.service.ProductService;
+import webapp8.webandtech.service.UserService;
+
+@RestController
+@CrossOrigin
+@RequestMapping("api/users")
+public class UserRestControler {
+
+
+}

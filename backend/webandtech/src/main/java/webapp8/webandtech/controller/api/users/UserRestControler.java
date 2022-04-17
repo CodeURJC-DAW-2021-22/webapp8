d956790b69d1f500e5d09b4e142b7386bae992f1
Guillermo <guillermo.mg.trabajo@gmail.com>
2022-04-17 22:40:28 +0200

    fix

--- backend/webandtech/src/main/java/webapp8/webandtech/controller/api/users/UserRestControler.java
+++ backend/webandtech/src/main/java/webapp8/webandtech/controller/api/users/UserRestControler.java
@@ -19,7 +19,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.hibernate.engine.jdbc.BlobProxy;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
